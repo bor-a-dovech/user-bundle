6201adbd25283d4dc56855e3b14575bab742f2b1
Sergey Borodinov <borodovich666@gmail.com>
2022-06-22 17:06:18 +0300

    работа с документами и водяными знаками

--- src/Service/StringService.php
+++ src/Service/StringService.php
@@ -18,4 +18,34 @@ class StringService
         json_decode($string);
         return json_last_error() === JSON_ERROR_NONE;
     }
+
+    /**
+     * Объединить параметры в путь, убрав дублирующиеся слеши.
+     * Результат без слешей в начале и в конце.
+     *
+     * @return string
+     */
+    public function createPath() {
+        $args = func_get_args();
+        $paths = array();
+        foreach ($args as $arg) {
+            $paths = array_merge($paths, (array)$arg);
+        }
+        $paths = array_map(function($element) {
+            return trim($element, "/");
+        }, $paths);
+        $paths = array_filter($paths);
+        return join('/', $paths);
+    }
+
+    /**
+     * Объединить параметры в путь, который начинается со слеша.
+     *
+     * @param mixed ...$args
+     * @return string
+     */
+    public function createSlashedPath(...$args)
+    {
+        return '/' . $this->createPath(...$args);
+    }
 }
